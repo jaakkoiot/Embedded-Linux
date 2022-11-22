8158fbefa1b7e33a3b283c41757cd27f5ad27c0e
jaakkoiot <jaakko.nahkala@metropolia.fi>
2022-11-22 23:21:21 +0200

    reduce maximum range for user input

--- c_progs/dice/input_parser.h
+++ c_progs/dice/input_parser.h
@@ -9,7 +9,7 @@
 //maximum input line is 64 characters, although no input of that length is actually respected in the program
 #define LINE 64
 //maximum data unit of the program is 32 767
-#define MAX SHRT_MAX
+#define MAX (SHRT_MAX - 2)
 
 //to-do: move range-validation of the user's input inside the read-function with passed pointers to already populated parameters -> cleaner main.c and more efficient
 uint16_t read_positive_integer();
