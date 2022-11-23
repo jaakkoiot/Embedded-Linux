e86b4047940dfd1360915a0278991cc5b8c87b15
jaakkoiot <jaakko.nahkala@metropolia.fi>
2022-11-23 22:30:08 +0200

    explain the implicit execution

--- c_progs/dice/compact.mk
+++ c_progs/dice/compact.mk
@@ -5,6 +5,8 @@ EXE=Dice
 OBJECTS=dice.o input_parser.o main.o
 
 #magic with wildcards & implicit rules:
+#implicit rules state to create each object from a c-file with the same name
+#instead of a ariable for the program name, $@ could be used for the output name 'compact'
 compact: $(OBJECTS)
 	$(CC) -o $(EXE) $^
 
