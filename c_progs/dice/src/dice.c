2949732febec43398cdbc3c7d1ab3c3797a029bd
jaakkoiot <jaakko.nahkala@metropolia.fi>
2022-11-28 20:46:21 +0200

    add comment on rand arr logic

--- c_progs/dice/src/dice.c
+++ c_progs/dice/src/dice.c
@@ -16,17 +16,18 @@ void generate(uint16_t c, uint16_t ma, uint16_t mi) {
 		checkmap[i] = false;
 	}
 
-	//seed rand
+	//seed rand with current system time
 	srand(time(NULL));
 
 	do {
-		//generate number between min - max
+		//generate number between min - max, it is still a candidate without validation (enforce unique)
 		num = (rand() % (max - min + 1)) + min;
-
 		//check the number based on index of the check map
 		if (checkmap[(num - min)] == false) {
+			//if checkmap indicates the value is not present -> program attempts to copy it to the array in a safe manner: if it succeeds the checkmap is changed to true on position
 			if(memcpy_safe(&arr[inclusions],sizeof(arr),&num,sizeof(uint16_t))){
 				checkmap[(num - min)] = true;
+				//inclusions is the final, incrementing index of the resulting array
 				inclusions++;
 			}
 		}
@@ -54,7 +55,7 @@ int memcpy_safe(void* dst, size_t dst_size, void* src, size_t src_size){
 	}
 	
 	if (src == NULL || dst_size < src_size){
-        	/* zeroe the destination buffer */
+        	/* zero the destination buffer */
         	memset(dst, 0, dst_size);
 		
 		return 0;
