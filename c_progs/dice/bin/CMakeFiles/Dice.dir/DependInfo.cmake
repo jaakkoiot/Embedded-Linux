9cbe7bd0cd8bc12a4583a9dcb97fa24a96039f2b
jaakkoiot <jaakko.nahkala@metropolia.fi>
2022-11-28 20:27:30 +0200

    add generated makefile in bin

---
+++ c_progs/dice/bin/CMakeFiles/Dice.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jaakko/c_progs/dice/src/dice.c" "CMakeFiles/Dice.dir/src/dice.c.o" "gcc" "CMakeFiles/Dice.dir/src/dice.c.o.d"
+  "/home/jaakko/c_progs/dice/src/input_parser.c" "CMakeFiles/Dice.dir/src/input_parser.c.o" "gcc" "CMakeFiles/Dice.dir/src/input_parser.c.o.d"
+  "/home/jaakko/c_progs/dice/src/main.c" "CMakeFiles/Dice.dir/src/main.c.o" "gcc" "CMakeFiles/Dice.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ c_progs/dice/bin/CMakeFiles/Dice.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dice.dir/src/dice.c.o"
+  "CMakeFiles/Dice.dir/src/dice.c.o.d"
+  "CMakeFiles/Dice.dir/src/input_parser.c.o"
+  "CMakeFiles/Dice.dir/src/input_parser.c.o.d"
+  "CMakeFiles/Dice.dir/src/main.c.o"
+  "CMakeFiles/Dice.dir/src/main.c.o.d"
+  "Dice"
+  "Dice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Dice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
