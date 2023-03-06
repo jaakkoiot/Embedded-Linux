988cad51e7dc19ba7c675c5f74e4e01541601465
Jaakko Nahkala <74764860+jaakkoiot@users.noreply.github.com>
2023-03-06 09:13:35 +0200

    Update dice.h

--- c_progs/dice/inc/dice.h
+++ c_progs/dice/inc/dice.h
@@ -5,14 +5,12 @@
 //maximum array sie of the program is 32 767 
 #define ARR_MAX SHRT_MAX
 
-//bitfield-handling static inline functions
-void SET(uint16_t BF, uint16_t N);
+//bitfield-handling helper functions:
+void SET(uint16_t BF, uint16_t N);    //set given bit in the bitfield
+void CLR(uint16_t BF, uint16_t N);    //clear given bit in the bitfield
+bool IS_SET(uint16_t BF, uint16_t N);  //check whether a specific bit is set in the bitfield
 
-void CLR(uint16_t BF, uint16_t N);
-
-bool IS_SET(uint16_t BF, uint16_t N);
-
-/**16-bit positive random generator with random order of elements, just because. Uses dynamic memory allocation inside -> to-do: change the passed parameters to pointers?**/
+/**16-bit positive random generator with random order of elements, just because. Uses dynamic memory allocation inside**/
 void generate(const uint16_t *count, const uint16_t *min, const uint16_t *max);
 
 //safe memcpy validating destination size and src / dst pointers. Returns 1 on succesful operation, 0 on failure
